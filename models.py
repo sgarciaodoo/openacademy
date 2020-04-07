aee27e96f78a2bf6a8c171421b8e8c43fc98d5c5
sgarciaodoo <sergio.garcia@smartsolutia.com>
2020-04-07 18:03:30 -0500

    hola

--- models.py
+++ models.py
@@ -2,7 +2,17 @@
 
 from odoo import models, fields, api
 
+class Course(models.Model):
+    _name = 'openacademy.course'
+    _description = "OpenAcademy Courses"
+
+    name = fields.Char(string="Title", required=True)
+    description = fields.Text()
+
 # class openacademy(models.Model):
 #     _name = 'openacademy.openacademy'
 
 #     name = fields.Char()
+
+
+
