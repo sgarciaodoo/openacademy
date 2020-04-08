49f2cc3d186d44a5296a24feac040461ffb50785
sgarciaodoo <sergio.garcia@smartsolutia.com>
2020-04-08 01:03:19 -0500

    Hola

--- models.py
+++ models.py
@@ -9,8 +9,10 @@ class Course(models.Model):
     name = fields.Char(string="Title", required=True)
     description = fields.Text()
 
-    responsible_id = fields.Many2one('res.users',
-    ondelete='set null', string="Responsible", index=True)
+	responsible_id = fields.Many2one('res.users',
+        ondelete='set null', string="Responsible", index=True)
+    session_ids = fields.One2many(
+        'openacademy.session', 'course_id', string="Sessions")
 
 class Session(models.Model):
     _name = 'openacademy.session'
