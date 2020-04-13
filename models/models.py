35f857474d097973f562a8bf7d49191a7f55d212
sgarciaodoo <sergio.garcia@smartsolutia.com>
2020-04-13 01:43:38 -0500

    d

--- models/models.py
+++ models/models.py
@@ -50,9 +50,12 @@ class Session(models.Model):
 
     active = fields.Boolean(default=True)
     color = fields.Integer()
-	instructor_id = fields.Many2one('res.partner', string="Instructor",
+
+    instructor_id = fields.Many2one('res.partner', string="Instructor",
         domain=['|', ('instructor', '=', True),
                      ('category_id.name', 'ilike', "Teacher")])
+
+
     course_id = fields.Many2one('openacademy.course',
         ondelete='cascade', string="Course", required=True)
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
